5618bf8354161dd1680a07c591568fe1c9dc2932
ellisp <peter.ellis2013nz@gmail.com>
2015-08-01 20:41:46 +1200

    add extra comments to shiny app UI

--- _working/_output/0001-shiny-benefits/ui.R
+++ _working/_output/0001-shiny-benefits/ui.R
@@ -15,15 +15,17 @@ shinyUI(fluidPage(
          selectInput("cat", "Choose a type of classification of beneficiary", 
                      choices = unique(benefits$Category), selectize = FALSE),
          radioButtons("stack", "Type of chart", choices = c("Individual lines", "Stacked"),
-                      selected = "Individual lines")
+                      selected = "Individual lines"),
+         
+         HTML("<p>Dates refer to quarters commencing in the month on the horizontal axis (note that this differs from the original publication, which has the month the quarter <i>completes</i>).</p>")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
          dygraphOutput("TimeSeries"),
-         HTML("<p><i>Source: 
-   <a href = 'https://www.msd.govt.nz/about-msd-and-our-work/publications-resources/statistics/benefit/index.html#Datatables6).'>
-              Ministry of Social Development</a></i></p>")
+         HTML("<p>Source: 
+   <i><a href = 'https://www.msd.govt.nz/about-msd-and-our-work/publications-resources/statistics/benefit/index.html#Datatables6).'>
+              Ministry of Social Development</a></i>.  Graphic by <a href = 'http://ellisp.github.io'><i>Stats in the Wild<i></a>.</p>")
       )
    )
 ))
