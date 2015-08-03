b689f1180a55b5f535d1a7b150eaf007b604162c
Peter Ellis <peter.ellis2013nz@gmail.com>
2015-08-03 22:53:50 +1200

    add function to calculate FIBS Elo rating.

--- _working/0002_fibs_elo.R
+++ _working/0002_fibs_elo.R
@@ -7,7 +7,8 @@ library(scales)
 library(extrafont)
 
 
-
+#==================helper functions=====================
+# in blog, move this script to bottom
 fibs_p <- function(a, b, ml){
    # function to determine the expected probability of player a winning a bachgammon match
    # against player b of length ml, with a and b representing their FIBS Elo ratings
@@ -16,6 +17,45 @@ fibs_p <- function(a, b, ml){
 }
 
 
+fibs_scores <- function(a, b, winner = "a", ml = 1, axp = 500, bxp = 500){
+   # a is Elo rating of player A before match
+   # b is Elo rating of player B before match
+   # ml is match length
+   # axp is total match lengths (experience) of player a until this match
+   # bxp is total match lengths (experience) of player b until this match
+   # see http://www.fibs.com/ratings.html for formulae
+   
+   # calculate experience-correction multipliers:
+   multa <- ifelse(axp < 400, 5 - ((axp + ml) / 100), 1)
+   multb <- ifelse(bxp < 400, 5 - ((axp + ml) / 100), 1)
+   
+   # probability of A winning:
+   winproba <- fibs_p(a = a, b = b, ml = ml)
+   
+   # match value (points to be distributed between the two players):
+   
+   matchvalue <- 4 * sqrt(ml)
+   
+   # who gets them?:
+   if(winner == "b"){
+      a <- a - matchvalue * winproba * multa
+      b <- b + matchvalue * winproba * multb
+   } else {
+      a <- a + matchvalue * (1 - winproba) * multa 
+      b <- b - matchvalue * (1- winproba) * multb
+   }
+   
+   return(list(a = a, b = b, axp = axp + ml, bxp = bxp + ml))
+}
+
+# test against "baptism by fire" example at http://www.fibs.com/ratings.html:
+round(fibs_scores(a = 1500, b = 1925, ml = 7, axp = 0, bxp = 10000, winner = "a")$a, 2) # should be 1540.95
+
+
+
+
+
+#================heat maps showing probability of winning
 a <- b <- seq(from = 1000, to = 2000, by = 5)
 
 mat <- expand.grid(a, b) %>%
@@ -48,3 +88,26 @@ png("../img/0002-ml5-23-elo.png", res * 10, res * 5, res = res, bg = "transparen
    print(direct.label(p1), vp = vp1)
    print(direct.label(p2), vp = vp2)
 dev.off()
+
+#================simulations of how long it takes for scores to stablise================
+
+
+# two people playing eachother 5 point games with 0.6 chance of winning
+
+
+
+
+
+# six people - 3 good, 3 bad - 5 point games 0.6 chance of good people winning
+
+
+
+
+
+
+# chance of winning changes according to an ARIMA process, on a logistic transformation dimension
+
+
+
+
+
