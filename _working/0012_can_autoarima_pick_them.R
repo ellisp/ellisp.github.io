648ef3d7e05ca20fa854846fc4073528418660d1
ellisp <peter.ellis2013nz@gmail.com>
2015-09-23 08:10:14 +1200

    generalise arima fitting to a function

--- _working/0012_can_autoarima_pick_them.R
+++ _working/0012_can_autoarima_pick_them.R
@@ -9,59 +9,75 @@ font.add.google("Poppins", "myfont")
 showtext.auto()
 theme_set(theme_light(base_family = "myfont"))
 
-# set.seed(123)
-# true_series <- arima.sim(model = list(ar = c(0.5, -0.1), ma = c(-0.3, 0.2)), n = 5000)
-# 
-# ar_series <- arma(true_series, order = c(4, 1), include.intercept = FALSE)
-# ma_series <- arma(true_series, order = c(1, 4), include.intercept = FALSE)
-# 
-# ar_series
-# ma_series
-
-
-# #-------------similar ACF, different models----------------
-# set.seed(123)
-# series1 <- arima.sim(model = list(ar = c(0.0215, 0.212, 0.033,-0.041), ma = 0.16), n = 500)
-# series2 <- arima.sim(model = list(ar = -0.05, ma = c(0.236, 0.225, .086, .014)), n = 500)
-# par(mfrow = c(1, 2))
-# acf(series1, type = "partial")
-# acf(series2, type = "partial")
-
-
 #-------------------how good is auto.arima at picking order of the ARMA process------------
-reps <- 1000
-n <- 2 ^ (1:10) * 10
-results <- data.frame(n = rep(n, each = reps),
+arima_fit_sim <- function(model, 
+                          correct_params, 
+                          n = 2 ^ (1:13) * 10, 
+                          reps = 10,
+                          stepwise = FALSE,
+                          verbose = TRUE){
+   require(forecast)
+   if(verbose){require(dplyr)}
+   
+   results <- data.frame(n = rep(n, each = reps),
                       correct = FALSE,
                       fitted = "",
                       stringsAsFactors = FALSE)
-counter <- 0
+   counter <- 0
 
-for(j in 1:length(n)){
-   message(n[j])
-   for(i in 1:reps){
-      counter <- counter + 1
-      true_series <- arima.sim(model = list(ar = c(0.5, -0.1), ma = c(-0.3, 0.2)), n = n[j])
-      fit <- auto.arima(true_series, stepwise = FALSE)
-      results[counter, "fitted"] <- paste(names(fit$coef), collapse = " ")
-      if(length(coef(fit)) == 4 && sum(names(fit$coef) %in% c("ar1", "ar2", "ma1", "ma2")) == 4 ){
-         results[counter, "correct"] <- TRUE
+   for(j in 1:length(n)){
+      if(verbose){message(n[j])}
+      for(i in 1:reps){
+         counter <- counter + 1
+         true_series <- arima.sim(model, n = n[j])
+         fit <- auto.arima(true_series, stepwise = stepwise)
+         results[counter, "fitted"] <- paste(names(fit$coef), collapse = " ")
+         if(length(coef(fit)) == length(correct_params) && sum(names(fit$coef) %in% correct_params) == length(correct_params) ){
+            results[counter, "correct"] <- TRUE
+            
+         }   else {
+            results[counter, "correct"] <- FALSE
+         }
          
-      }   else {
-         results[counter, "correct"] <- FALSE
       }
-      
    }
-}
+   
+   if(verbose){
+      print(
+         results %>%
+            group_by(n) %>%
+            summarise(correct = sum(correct) / length(correct) * 100)
+         )
+   }
 
-results %>%
+   return(results)
+   }
+
+
+our_reps <- 5
+
+results_ar1 <- arima_fit_sim(model = list(ar = c(0.5)),
+                             correct_params = c("ar1"), 
+                             reps = our_reps,
+                             n = 2 ^ (1:8) * 10)
+
+results_ma1 <- arima_fit_sim(model = list(ma = c(0.5)),
+                             correct_params = c("ma1"), 
+                             reps = our_reps)
+
+
+results_arma22 <- arima_fit_sim(model = list(ar = c(0.5, -0.1), ma = c(-0.3, 0.2)),
+                                correct_params = c("ar1", "ar2", "ma1", "ma2"), 
+                                reps = our_reps)
+
+results_arma33 <- arima_fit_sim(model = list(ar = c(0.5, -0.1, 0.3), ma = c(-0.3, 0.2, 0.4)),
+                                correct_params = c("ar1", "ar2", "ar3", "ma1", "ma2", "ma3"), 
+                                reps = our_reps)
+
+
+
+results_ar1 %>%
    group_by(n) %>%
    summarise(correct = sum(correct) / length(correct) * 100)
 
-
-results %>%
-   group_by(fitted) %>%
-   summarise(count = length(fitted)) %>%
-   arrange(count) %>%
-   data.frame()
 
