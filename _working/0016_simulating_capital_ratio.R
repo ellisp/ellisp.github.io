b56b1ad7ed290056d02a9fd275627673d1632fa8
ellisp <peter.ellis2013nz@gmail.com>
2015-10-11 11:55:06 +1300

    working version of toy economic model for capital ratio

--- _working/0016_simulating_capital_ratio.R
+++ _working/0016_simulating_capital_ratio.R
@@ -1,32 +1,51 @@
-beta
-
-capital_income_ratio = .12 / .02
-
-
-
-
-capital stock = 6 * income
-
-savings        = 0.12 * income
-
-incomegrowth
-
-
-capital/income = savings / incomegrowth
-
-income = savings / 0.12
-capital stock = 6 * savings / 0.12
+library(ggplot2)
+library(scales)
+library(dplyr)
+library(gridExtra)
+library(tidyr)
+library(mbie)
+#======================exogenous saving rate and growth in production==========
+duration <- 1000
+economy <- data.frame(
+   year = 1:duration,
+   capital = 1000,
+   production = 1000,
+   saving_rate = 0.12 + arima.sim(model = list(ar = 0.9), n = duration) / 60,
+   growth = 1.02 + arima.sim(model = list(ar = 0.9), n = duration) / 70
+   )
 
 
-
-capital <- 0
-income <- 100000
-
-for(i in 1:1000){
-   capital <- capital + .12 *income
-   income <- income * 1.02
-   cat(i, round(income), round(capital),  round(capital / income, 1), "\n")
+for(i in 2:duration){
+   economy[i, "capital"] <- economy[i - 1, "capital"] + 
+      economy[i, "saving_rate"] * economy[i - 1, "production"]
+   
+   economy[i, "production"] <- economy[i - 1, "production"] * economy[i, "growth"]
    
 }
 
+# capital expressed as number of years of production:
+economy$CapIncRatio <- with(economy, capital / production)
 
+p1 <- economy %>%
+   mutate(LogProduction = log10(production)) %>%
+   select(year, CapIncRatio, LogProduction) %>%
+   gather("variable", "value", -year) %>%
+   mutate(variable = gsub("CapIncRatio", "Capital as a proportion of a year's production", variable),
+          variable = gsub("LogProduction", "Production (logarithm)", variable)) %>%
+   ggplot(aes(x = year, y = value)) +
+   facet_wrap(~variable, ncol = 1, scales = "free_y") +
+   geom_line()
+
+p2 <- ggplot(economy, aes(x = year, y = saving_rate)) +
+   geom_line() +
+   scale_y_continuous("Savings as a percent of last year's production", label = percent)
+
+p3 <- ggplot(economy, aes(x = year, y = (growth - 1))) +
+   geom_line() +
+   scale_y_continuous("Growth in production", label = percent)
+
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(2, 2)))
+print(p1, vp = vplayout(1:2, 1))
+print(p3, vp = vplayout(1, 2))
+print(p2, vp = vplayout(2, 2))
