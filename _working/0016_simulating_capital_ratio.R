baf51d38968d123f036b5255c399d3c333d63898
ellisp <peter.ellis2013nz@gmail.com>
2015-10-11 13:02:06 +1300

    endogenous model

--- _working/0016_simulating_capital_ratio.R
+++ _working/0016_simulating_capital_ratio.R
@@ -4,6 +4,38 @@ library(dplyr)
 library(gridExtra)
 library(tidyr)
 library(mbie)
+#=================helper function============
+draw_plots <- function(economy){
+   # capital expressed as number of years of production:
+   economy$CapIncRatio <- with(economy, capital / production)
+   
+   p1 <- economy %>%
+      mutate(LogProduction = log10(production)) %>%
+      select(year, CapIncRatio, LogProduction) %>%
+      gather("variable", "value", -year) %>%
+      mutate(variable = gsub("CapIncRatio", "Capital as a proportion of a year's production", variable),
+             variable = gsub("LogProduction", "Production (logarithm)", variable)) %>%
+      ggplot(aes(x = year, y = value)) +
+      facet_wrap(~variable, ncol = 1, scales = "free_y") +
+      geom_line()
+   
+   p2 <- ggplot(economy, aes(x = year, y = saving_rate)) +
+      geom_line() +
+      scale_y_continuous("Savings as a percent of last year's production", label = percent)
+   
+   p3 <- ggplot(economy, aes(x = year, y = (growth - 1))) +
+      geom_line() +
+      scale_y_continuous("Growth in production", label = percent)
+   
+   grid.newpage()
+   pushViewport(viewport(layout = grid.layout(2, 2)))
+   print(p1, vp = vplayout(1:2, 1))
+   print(p3, vp = vplayout(1, 2))
+   print(p2, vp = vplayout(2, 2))
+}
+
+
+
 #======================exogenous saving rate and growth in production==========
 duration <- 1000
 economy <- data.frame(
@@ -23,29 +55,44 @@ for(i in 2:duration){
    
 }
 
-# capital expressed as number of years of production:
-economy$CapIncRatio <- with(economy, capital / production)
 
-p1 <- economy %>%
-   mutate(LogProduction = log10(production)) %>%
-   select(year, CapIncRatio, LogProduction) %>%
-   gather("variable", "value", -year) %>%
-   mutate(variable = gsub("CapIncRatio", "Capital as a proportion of a year's production", variable),
-          variable = gsub("LogProduction", "Production (logarithm)", variable)) %>%
-   ggplot(aes(x = year, y = value)) +
-   facet_wrap(~variable, ncol = 1, scales = "free_y") +
-   geom_line()
 
-p2 <- ggplot(economy, aes(x = year, y = saving_rate)) +
-   geom_line() +
-   scale_y_continuous("Savings as a percent of last year's production", label = percent)
+#===================more complex endogenous model================
+duration <- 1000
+economy <- data.frame(
+   year = 1:duration,
+   capital = 1000,
+   labour = 1000,
+   production = 1000,
+   saving_rate = 0.12,
+   growth = 1.02,
+   labour_growth = 1.01 + arima.sim(model = list(ar = 0.9), n = duration) / 200,
+   randomness1 = 1 + arima.sim(model = list(ar = 0.9), n = duration) / 300,
+   randomness2 = arima.sim(model = list(ar = 0.9), n = duration) / 100
+)
 
-p3 <- ggplot(economy, aes(x = year, y = (growth - 1))) +
-   geom_line() +
-   scale_y_continuous("Growth in production", label = percent)
+for(i in 2:duration){
+   economy[i, "saving_rate"] <- economy[1, "saving_rate"] +
+      (economy[i - 1, "growth"] - 1.02) * rnorm(1, 3, 0.2) +
+      economy[i, "randomness2"]
+   
+   economy[i, "capital"] <- economy[i - 1, "capital"] + 
+      economy[i, "saving_rate"] * economy[i - 1, "production"]
+   economy[i, "labour"] <- economy[i - 1, "labour"] * 
+      economy[i, "labour_growth"]
+   
+   K <- economy[i, "capital"] # for ease of use
+   L <- economy[i, "labour"]
+   Y <- 1.95 * K ^ 0.3 * L ^ 0.6 * economy[i, "randomness1"]
+   economy[i, "production"] <- Y
+   economy[i, "growth"] <- Y / economy[i - 1, "production"] 
+   
+}
 
-grid.newpage()
-pushViewport(viewport(layout = grid.layout(2, 2)))
-print(p1, vp = vplayout(1:2, 1))
-print(p3, vp = vplayout(1, 2))
-print(p2, vp = vplayout(2, 2))
+draw_plots(economy)
+head(economy, 10)
+
+economy %>%
+   mutate(lag_growth = c(NA, growth[-duration])) %>%
+   filter(!is.na(lag_growth)) %>%
+   ggplot(aes(x = lag_growth, y = saving_rate)) + geom_path() + geom_smooth(method = "lm")
