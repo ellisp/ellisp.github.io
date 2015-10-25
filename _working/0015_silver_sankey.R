d68e7fcd1473125c9a45e6ea8b82bf400eb51aac
ellisp <peter.ellis2013nz@gmail.com>
2015-10-25 13:46:55 +1300

    ready for drafting actual blog on silver

--- _working/0015_silver_sankey.R
+++ _working/0015_silver_sankey.R
@@ -1,31 +1,55 @@
-   library(riverplot)
-   library(RColorBrewer)
-   library(dplyr)
-   library(grid)
-   library(RSvgDevice)
-   library(showtext) # for fonts
-   library(stringr)
-      
-   font.add.google("Poppins", "myfont")
-   showtext.auto()
+library(riverplot)
+library(RColorBrewer)
+library(dplyr)
+library(grid)
+library(showtext)    # for fonts
+library(stringr)
    
-   pal <- c("orange", "cyan", "grey95")
+font.add.google("Poppins", "myfont")
+showtext.auto()
+
+
+silver <- function(node_values, edge_values, title, 
+                   pal = c("orange", "cyan", "grey95"), family = "myfont",
+                   cal_width = 650){
+   # @node_values - the number of production / consumption / travel to be added
+   #   to America, Western Europe, Levant, Baltic, Cape, Japan, SE Asia, Atlantic, Pacific, Sea of Japan
+   # @edge_values - the width of the connections in this order:
+   #    America to Pacific to South East Asia
+   #    America to Atlantic to Western Europe
+   #    Western Europe to Baltic to South East Asia
+   #    Western Europe to Levant to South East Asia
+   #    Western Europe to Cape to South East Asia
+   #    Japan to Sea of Japan to South East Asia
+   # @title title for plot
+   # @pal vector of colours for producers, consumers, and travel routes respectively
+   # @family font family to use
+   # @cal_width width of an invisible white ribbon to draw, implicitly the maximum, so scale is constant for multiple plots
+   
+   # edge_values comes in as just a single number and we need two edges for each (eg America to Pacific to SE Asia is two edges)
+   edge_values <- rep(edge_values, each = 2)
+   
+   # we don't know how much of the silver that goes via the Baltic and the Levant
+   # actually gets to Asia (as opposed to staying in middle East and Russia),
+   # so to represent this we halve the width of those two edges:
+   edge_values[ c(6, 8)] <- edge_values[ c(6, 8)] / 2
    
    nodes <- data.frame(
-      x = c(2, 4, 6, 5, 4.7, 9, 8, 3,   5, 8.5),
-      y = c(3, 6, 5, 7, 4,   5, 3, 4.5, 0,  4),
-      ID = LETTERS[1:10],
-      labels = c("America", "Western Europe", "Levant", "Baltic", "Cape of\nGood Hope", "Japan", "South &\nEast Asia", "Atlantic", "Pacific", ""),
-      amount = c(368,        158.5,            38,       56,       15.5,                 "unknown",     "91-126",          268,       "17-51", "59.3"),
-      col = pal[c(1, 2, 3, 3, 3, 1, 2, 3, 3, 3)],
+      x = c(2, 4, 6, 5, 4.7, 9, 8, 3,   5, 8.5, 2, 9),
+      y = c(3, 6, 5, 7, 4,   5, 3, 4.5, 0, 4,   8, 8),
+      ID = LETTERS[1:12],
+      labels = c("America", "Western Europe", "Levant", "Baltic", "Cape of\nGood Hope", "Japan", "South &\nEast Asia", 
+                 "Atlantic", "Pacific", "", "", ""),
+      amount = c(node_values, "", ""),
+      col = c(pal[c(1, 2, 3, 3, 3, 1, 2, 3, 3, 3)], "white", "white"),
       stringsAsFactors = FALSE) %>%
       mutate(labels = paste(labels, amount, sep = "\n"),
              labels = gsub("\n59.3", "59.3", labels))
    
    edges <- data.frame(
-      N1 =    c("A", "I", "A", "H", "B", "B", "B", "D", "C",  "E",  "F", "J"),
-      N2 =    c("I", "G", "H", "B", "D", "C", "E", "G", "G",  "G",  "J", "G"),
-      Value = c(34,  34,  268,  268, 56,  38,  56, 38,   15.5, 15.5, 59.3, 59.3)
+      N1 =    c("A", "I", "A", "H", "B", "D", "B", "C", "B", "E",  "F", "J", "K"),
+      N2 =    c("I", "G", "H", "B", "D", "G", "C", "G", "E", "G",  "J", "G", "L"),
+      Value = c(edge_values, cal_width)
    )
    
    p <- makeRiver(nodes, edges)
@@ -33,16 +57,39 @@
    my_style$textcol <- "grey20"
    my_style$srt = 0
    
-   devSVG("test.svg", 8, 5)
-   par(family = "myfont")
-   
-   plot(p, default_style = my_style, plot_area = 0.8)
-   grid.text(x = 0.5, y = 0.97, "Intercontinental flows of silver, kilograms per year, 1600 to 1650",
-             gp = gpar(fontfamily = "myfont", cex = 1.5))
+   par(family = family)
+   plot(p, default_style = my_style, plot_area = 0.9)
+   grid.text(x = 0.5, y = 0.94, title,
+             gp = gpar(fontfamily = family, cex = 1.5))
 #    grid.text(x = 0.77, y = 0.1, "Kilogrammes per year",
-#              gp = gpar(fontfamily = "myfont", cex = 1))
-   grid.text(x = 0.15, y = 0.15, str_wrap("Source: de Vries (2003), reproduced in Findlay and O'Rourke 'Power and Plenty'", 30),
-             gp = gpar(fontfamily = "myfont", fontface = "italic", cex = 0.7))
+#              gp = gpar(fontfamily = family, cex = 1))
+   grid.text(x = 0.15, y = 0.15, str_wrap("Source: de Vries (2003), reproduced in Findlay and O'Rourke (2007) 'Power and Plenty'", 30),
+             gp = gpar(fontfamily = family, fontface = "italic", cex = 0.7))
    legend(7, 1, legend = c("Producer", "Consumer"), bty = "n", fill = pal[1:2], border = FALSE, cex=0.8)
-   dev.off()
+}
+
+
+png("_output/0015-silver-sankey/01.png", 800, 500, res = 100)
+   silver(
+      node_values = c(368, 158.5, 38, 56, 15.5, "?", "91-126", 268, "17-51", "59.3"), 
+      edge_values = c(34,  268,  56,  38, 15.5, 59.3), 
+      title = "Intercontinental flows of silver, kilograms per year, 1600 to 1650")
+dev.off()
+
+png("_output/0015-silver-sankey/02.png", 800, 500, res = 100)
+silver(
+   node_values = c(650, 230, 60, 50, 160, "?", "175-211.2", 500, "15-51.2", "0"), 
+   edge_values = c(33.2,  500,  50,  60, 160, 0.00), 
+   title = "Intercontinental flows of silver, kilograms per year, 1725 to 1750")
+dev.off()
+
+wd <- setwd("_output/0015-silver-sankey/")
+system('"C:\\Program Files\\ImageMagick-6.9.1-Q16\\convert" -loop 0 -delay 250 *.png "silver.gif"')
+
+# move the asset over to where needed for the blog
+file.copy("silver.gif", "../../../img/0015-silver.gif", overwrite = TRUE)
+
+# back to working drive
+setwd(wd)
+
 
