fcbd3b7ffaf226b30aa75978d681e27531b13ac8
ellisp <peter.ellis2013nz@gmail.com>
2015-10-26 10:42:38 +1300

    working version of silver sankey blog post

--- _working/0015_silver_sankey.R
+++ _working/0015_silver_sankey.R
@@ -1,5 +1,4 @@
-library(riverplot)
-library(RColorBrewer)
+library(riverplot)   # for Sankey charts
 library(dplyr)
 library(grid)
 library(showtext)    # for fonts
@@ -10,8 +9,9 @@ showtext.auto()
 
 
 silver <- function(node_values, edge_values, title, 
-                   pal = c("orange", "cyan", "grey95"), family = "myfont",
+                   pal = c("orange", "cyan", "grey90"), family = "myfont",
                    cal_width = 650){
+   # Function that draws a Sankey chart of intercontinental silver flows
    # @node_values - the number of production / consumption / travel to be added
    #   to America, Western Europe, Levant, Baltic, Cape, Japan, SE Asia, Atlantic, Pacific, Sea of Japan
    # @edge_values - the width of the connections in this order:
@@ -35,6 +35,9 @@ silver <- function(node_values, edge_values, title,
    edge_values[ c(6, 8)] <- edge_values[ c(6, 8)] / 2
    
    nodes <- data.frame(
+      
+      # The X and Y coordinates of the nodes (Americas, Atlantic, etc) are 
+      # just chosen by hand:
       x = c(2, 4, 6, 5, 4.7, 9, 8, 3,   5, 8.5, 2, 9),
       y = c(3, 6, 5, 7, 4,   5, 3, 4.5, 0, 4,   8, 8),
       ID = LETTERS[1:12],
@@ -47,6 +50,7 @@ silver <- function(node_values, edge_values, title,
              labels = gsub("\n59.3", "59.3", labels))
    
    edges <- data.frame(
+      # Each N1 - N2 pair connects two nodes eg A-I connects America to Atlantic:
       N1 =    c("A", "I", "A", "H", "B", "D", "B", "C", "B", "E",  "F", "J", "K"),
       N2 =    c("I", "G", "H", "B", "D", "G", "C", "G", "E", "G",  "J", "G", "L"),
       Value = c(edge_values, cal_width)
@@ -61,28 +65,29 @@ silver <- function(node_values, edge_values, title,
    plot(p, default_style = my_style, plot_area = 0.9)
    grid.text(x = 0.5, y = 0.94, title,
              gp = gpar(fontfamily = family, cex = 1.5))
-#    grid.text(x = 0.77, y = 0.1, "Kilogrammes per year",
-#              gp = gpar(fontfamily = family, cex = 1))
-   grid.text(x = 0.15, y = 0.15, str_wrap("Source: de Vries (2003), reproduced in Findlay and O'Rourke (2007) 'Power and Plenty'", 30),
+   grid.text(x = 0.15, y = 0.15, 
+             str_wrap("Source: de Vries (2003), reproduced in Findlay and O'Rourke (2007) 'Power and Plenty'", 30),
              gp = gpar(fontfamily = family, fontface = "italic", cex = 0.7))
-   legend(7, 1, legend = c("Producer", "Consumer"), bty = "n", fill = pal[1:2], border = FALSE, cex=0.8)
+   legend(7, 1, legend = c("Producer", "Trade route", "Consumer"), 
+          bty = "n", fill = pal[c(1, 3, 2)], border = FALSE, cex=0.8)
 }
 
-
+# draw the two images
 png("_output/0015-silver-sankey/01.png", 800, 500, res = 100)
    silver(
       node_values = c(368, 158.5, 38, 56, 15.5, "?", "91-126", 268, "17-51", "59.3"), 
       edge_values = c(34,  268,  56,  38, 15.5, 59.3), 
-      title = "Intercontinental flows of silver, kilograms per year, 1600 to 1650")
+      title = "Intercontinental flows of silver, tonnes per year, 1600 to 1650")
 dev.off()
 
 png("_output/0015-silver-sankey/02.png", 800, 500, res = 100)
 silver(
    node_values = c(650, 230, 60, 50, 160, "?", "175-211.2", 500, "15-51.2", "0"), 
    edge_values = c(33.2,  500,  50,  60, 160, 0.00), 
-   title = "Intercontinental flows of silver, kilograms per year, 1725 to 1750")
+   title = "Intercontinental flows of silver, tonnes per year, 1725 to 1750")
 dev.off()
 
+# combine them into an animated GIF
 wd <- setwd("_output/0015-silver-sankey/")
 system('"C:\\Program Files\\ImageMagick-6.9.1-Q16\\convert" -loop 0 -delay 250 *.png "silver.gif"')
 
