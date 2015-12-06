ea4c13b981c063337e0711c70e528626b3f8ad50
ellisp <peter.ellis2013nz@gmail.com>
2015-12-06 13:44:09 +1300

    analysis of exports and GDP

--- _working/0023-exports-gdp.R
+++ _working/0023-exports-gdp.R
@@ -5,18 +5,21 @@ library(dplyr)
 library(tidyr)
 library(GGally)   # for ggplot pairs plot
 library(showtext) # for fonts
+library(nlme)
 
 # import fonts
 font.add.google("Poppins", "myfont")
 showtext.auto()
-
+theme_set(theme_light(10, base_family = "myfont"))
 
 
 #-------------------data imports, first explore, and clean-----------------
-# Exports of goods and services (% of GDP)
-exports <- WDI(indicator = "NE.EXP.GNFS.ZS", end = 2015, start = 1960)
-# GDP per capita (constant 2000 US$)
-gdp <- WDI(indicator = "NY.GDP.PCAP.KD", end = 2015, start = 1960)
+if(!exists("exports")){
+   # Exports of goods and services (% of GDP)
+   exports <- WDI(indicator = "NE.EXP.GNFS.ZS", end = 2015, start = 1960)
+   # GDP per capita (constant 2000 US$)
+   gdp <- WDI(indicator = "NY.GDP.PCAP.KD", end = 2015, start = 1960)
+}
 
 both <- merge(exports, gdp) %>%
    rename(exports = NE.EXP.GNFS.ZS,
@@ -28,16 +31,16 @@ both <- merge(exports, gdp) %>%
 # let's look at 16 countries at a time
 all_countries <- unique(both$country)
 sampled <- both    %>%
-   filter(country %in% sample(all_countries, 16))
+   filter(country %in% sample(all_countries, 12))
    
 # connected scatter plot:
-sampled %>%
+p1 <- sampled %>%
    ggplot(aes(y = gdp, x = exports, colour = year)) +
    facet_wrap(~country, scales = "free") +
    geom_path()
 
 # univariate time series plots
-sampled %>%
+p2 <- sampled %>%
    gather(variable, value, -(iso2c:year)) %>%
    ggplot(aes(x = year, y = value)) +
    geom_line() +
@@ -62,7 +65,7 @@ country_sum <- both2 %>%
    summarise(gdp = mean(gdp),
              exports = mean(exports))
 
-country_sum %>%
+p3 <- country_sum %>%
    ggplot(aes(x = exports, y = gdp, label = country)) +
    geom_text(size = 3) +
    geom_smooth(method = "lm") +
@@ -80,9 +83,11 @@ first_values <- both2 %>%
    group_by(country) %>%
    filter(year == min(year)) %>%
    ungroup() %>%
-   rename(exports_starter = exports,
+   mutate(exports_starter = log(exports),
           first_year = year) %>%
    select(iso2c, exports_starter, first_year)
+
+p4 <- ggplot(both3, aes(x = exports_starter)) + geom_density()
 
 # all those individual time series have problem of non-stationarity and of course autocorrelation
 # so when we merge with the starting values of exports we also calculate first
@@ -92,17 +97,107 @@ both3 <- both2 %>%
    arrange(country, year) %>%
    group_by(country) %>%
    mutate(exports_g = c(NA, diff(log(exports))),
-          gdp_g = c(NA, diff(log(gdp)))) %>%
-   ungroup()
+          gdp_g = c(NA, diff(log(gdp))),
+          exports_lag = lag(exports_g)) %>%
+   ungroup() %>%
+   filter(!is.na(gdp_g)) %>%
+   filter(!is.na(exports_lag))
 
+head(both3)
 
 all_countries2 <- unique(both3$country)
 
-both3 %>%
-   filter(country %in% sample(all_countries2, 15)) %>%
+set.seed(123)
+sampled <- both3 %>%
+   filter(country %in% sample(all_countries2, 12)) 
+
+p5 <- sampled %>%
    select(country, year, exports_g, gdp_g) %>%
    gather(variable, value, -(country:year)) %>%
    ggplot(aes(x = year, y = value)) +
    geom_line() +
    facet_wrap(~ country + variable, scales = "free_y")
+
+p6 <- sampled %>%
+   select(country, year, exports_g, gdp_g) %>%
+   ggplot(aes(x = exports_g, y = gdp_g, colour = year)) +
+   geom_smooth(colour = "red", method = "lm") +
+   geom_path() +
+   geom_point() +
+   facet_wrap(~ country, scales = "free") +
+   labs(x = "change in logarithm of exports share of GDP",
+        y = "change in logarithm of GDP per capita, constant 2000 USD",
+        title = "Changing importance of exports and GDP growth, selected countries")
+
+
+
+# we start with some exploratory fixed effects models
+# fixed effects model
+model0 <- lm(gdp_g ~ country +  exports_g, data = both3)
+summary(model0)
+anova(model0)
+AIC(model0)
+
+# alternative spec of fixed effects model
+model1 <- lm(gdp_g ~ ordered(first_year) + exports_g + exports_starter, data = both3)
+summary(model1)
+anova(model1)
+AIC(model1)
+
+model2 <- lme(fixed = gdp_g ~ ordered(first_year) + exports_g + exports_starter,
+              random = ~ 1 | country, 
+              data = both3)
+
+summary(model2)$tTable
+anova(model2)
+ACF(model2) # special ACF function for objects of class lme
+AIC(model2)
+
+model3 <- lme(fixed = gdp_g ~ ordered(first_year) + exports_g + exports_starter,
+              random = ~ 1 | country, 
+              correlation = corAR1(),
+              data = both3)
+anova(model3)
+AIC(model3)
+
+# now that we have an appropriate error structure, the p values are further from zero
+# ie less significant.  We look at our four models, each one closer to what we 
+# really believe is a good fit, just focusing on the parameters of interest
+round(tail(summary(model0)$coefficients, 1), 3)
+round(tail(summary(model1)$coefficients, 2), 3)
+round(tail(summary(model2)$tTable, 2), 3)
+round(tail(summary(model3)$tTable, 2), 3)
+
+model4 <- lme(fixed = gdp_g ~ ordered(first_year) + exports_g + exports_lag + exports_starter,
+              random = ~ 1 | country, 
+              correlation = corAR1(),
+              data = both3)
+
+tail(round(summary(model4)$tTable, 3), 3)
+
+#----------------------save charts--------------
+
+svg("../img/0023-p1.svg", 9, 7)
+print(p1)
+dev.off()
+
+svg("../img/0023-p2.svg", 9, 7)
+print(p2)
+dev.off()
+
+svg("../img/0023-p3.svg", 8, 7)
+print(p3)
+dev.off()
+
+svg("../img/0023-p4.svg", 8, 7)
+print(p4)
+dev.off()
+
+svg("../img/0023-p5.svg", 9, 7)
+print(p5)
+dev.off()
+
+svg("../img/0023-p6.svg", 9, 7)
+print(p6)
+dev.off()
 
--- _working/00XX-m3.R
+++ _working/00XX-m3.R
@@ -1,43 +1,64 @@
-require(Mcomp)
+library(Mcomp)
+library(dplyr)
+library(seasonal)
+
+# set path to X13-SEATS and check it's working
+Sys.setenv(X13_PATH = "c:/winx13/x13as")
+checkX13()
+
 nseries <- length(M3)
 
-theta <- as.matrix(M3Forecast$THETA)
-fpro <- as.matrix(M3Forecast$ForecastPro)
-fcx <- as.matrix(M3Forecast$ForcX)
-bjauto <- as.matrix(M3Forecast$`B-J auto`)
-ab1 <- as.matrix(M3Forecast$AutoBox1)
-ab2 <- as.matrix(M3Forecast$AutoBox2)
-ab3 <- as.matrix(M3Forecast$AutoBox3)
+nseries <- 50
 
-ets1 <- aarima <- hybrid <- matrix(NA, nrow = nseries, ncol = 18)
+# define number of new methods
+number_methods <- 7 + 3 # 7 original, 3 new
+
+theta <- as.matrix(M3Forecast$THETA)[1:nseries, ]
+fpro <- as.matrix(M3Forecast$ForecastPro)[1:nseries, ]
+fcx <- as.matrix(M3Forecast$ForcX)[1:nseries, ]
+bjauto <- as.matrix(M3Forecast$`B-J auto`)[1:nseries, ]
+ab1 <- as.matrix(M3Forecast$AutoBox1)[1:nseries, ]
+ab2 <- as.matrix(M3Forecast$AutoBox2)[1:nseries, ]
+ab3 <- as.matrix(M3Forecast$AutoBox3)[1:nseries, ]
+
+ets1 <- aarima <- hybrid <- seats <- x11 <- matrix(NA, nrow = nseries, ncol = 18)
 
 for(i in 1:nseries)
 {
    print(i)
-   ets1[i,] <- forecast(ets(M3[[i]]$x), h = 18, PI = FALSE)$mean
-   aarima[i,] <- forecast(auto.arima(M3[[i]]$x), h = 18)$mean
-   hybrid[i,] <- 0.5*(aarima[i, ] + ets1[i, ])
+   ets1[i, ] <- forecast(ets(M3[[i]]$x), h = 18, PI = FALSE)$mean
+   aarima[i, ] <- forecast(auto.arima(M3[[i]]$x), h = 18)$mean
+   hybrid[i, ] <- 0.5 * (aarima[i, ] + ets1[i, ])
+   
+   if(M3[[i]]$period != "YEARLY"){
+      m <- seas(M3[[i]]$x, forecast.maxlead = 18)
+   } else {
+      m <- seas(M3[[i]]$x, regression.aictest = NULL, forecast.maxlead = 18)
+      m <- seas(M3[[i]]$x, regression.aictest = NULL)
+   }
+   seats[i, ] <- series(m, "forecast.forecasts")[ , 1]      
+   
 }
 
 # Compute accuracy
 mase <- mape <- smape <- matrix(NA, nrow = 10, ncol = nseries)
-f <- matrix(NA, nrow = 10, ncol = 18)
+f <- matrix(NA, nrow = number_methods, ncol = 18)
 for(i in 1:nseries)
 {
    x <- M3[[i]]$xx
    n <- length(x)
-   f[1,1:n] <- theta[i, 1:n]
-   f[2,1:n] <- fpro[i, 1:n]
-   f[3,1:n] <- fcx[i, 1:n]
-   f[4,1:n] <- bjauto[i, 1:n]
-   f[5,1:n] <- ab1[i, 1:n]
-   f[6,1:n] <- ab2[i, 1:n]
-   f[7,1:n] <- ab3[i, 1:n]
-   f[8,1:n] <- ets1[i, 1:n]
-   f[9,1:n] <- aarima[i, 1:n]
-   f[10,1:n] <- hybrid[i, 1:n]
+   f[1, 1:n] <- theta[i, 1:n]
+   f[2, 1:n] <- fpro[i, 1:n]
+   f[3, 1:n] <- fcx[i, 1:n]
+   f[4, 1:n] <- bjauto[i, 1:n]
+   f[5, 1:n] <- ab1[i, 1:n]
+   f[6, 1:n] <- ab2[i, 1:n]
+   f[7, 1:n] <- ab3[i, 1:n]
+   f[8, 1:n] <- ets1[i, 1:n]
+   f[9, 1:n] <- aarima[i, 1:n]
+   f[10, 1:n] <- hybrid[i, 1:n]
    scale <- mean(abs(diff(M3[[i]]$x, lag = frequency(x))))
-   for(j in 1:10)
+   for(j in 1:number_methods)
    {
       mape[j, i] <- mean(abs((x - f[j, 1:n]) / x)) * 100
       smape[j, i] <- mean(abs(x - f[j, 1:n]) / (abs(x) + abs(f[j, 1:n]))) * 200
@@ -46,13 +67,17 @@ for(i in 1:nseries)
 }
 
 # All series
-m3table <- matrix(NA, nrow = 10, ncol = 3)
+m3table <- matrix(NA, nrow = number_methods, ncol = 3)
 m3table[,1] <- rowMeans(mape, na.rm = TRUE)
 m3table[,2] <- rowMeans(smape)
 m3table[,3] <- rowMeans(mase)
-rownames(m3table) <- c("Theta", "ForecastPro", "ForecastX", "BJauto",
+m3table <- as.data.frame(m3table)
+
+names(m3table) <- c("MAPE", "sMAPE", "MASE")
+
+m3table$method <- c("Theta", "ForecastPro", "ForecastX", "BJauto",
                        "Autobox1", "Autobox2", "Autobox3",
                        "ETS", "AutoARIMA", "Hybrid")
-colnames(m3table) <- c("MAPE", "sMAPE", "MASE")
-j <- order(m3table[,3])
-round(m3table[j,], 2)
+
+m3table %>%
+   arrange(MASE)
