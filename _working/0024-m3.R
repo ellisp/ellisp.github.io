e80a629a9653f087896ae3426712ce27343fe395
ellisp <peter.ellis2013nz@gmail.com>
2015-12-21 18:48:11 +1300

    working version of X13 M3 blog post

--- _working/0024-m3.R
+++ _working/0024-m3.R
@@ -9,6 +9,8 @@ library(seasonal)
 library(showtext)
 library(ggplot2)
 library(scales)
+library(RColorBrewer)
+
 
 # import fonts
 font.add.google("Poppins", "myfont")
@@ -22,8 +24,8 @@ checkX13()
 
 nseries <- length(M3)
 
-# nseries <- 500 # for development
 
+# forecasts from the original competition
 theta <- as.matrix(M3Forecast$THETA)[1:nseries, ]
 fpro <- as.matrix(M3Forecast$ForecastPro)[1:nseries, ]
 fcx <- as.matrix(M3Forecast$ForcX)[1:nseries, ]
@@ -32,8 +34,12 @@ ab1 <- as.matrix(M3Forecast$AutoBox1)[1:nseries, ]
 ab2 <- as.matrix(M3Forecast$AutoBox2)[1:nseries, ]
 ab3 <- as.matrix(M3Forecast$AutoBox3)[1:nseries, ]
 
-ets1 <- aarima <- theta_fc <- hybrid2a <- hybrid2b <- hybrid2c <- hybrid3 <- seats <- matrix(NA, nrow = nseries, ncol = 18)
+# set up matrices to hold the forecasts from new methods
+ets1 <- aarima <- theta_fc <- hybrid2a <- hybrid2b <- 
+   hybrid2c <- hybrid3 <- seats <- 
+   matrix(NA, nrow = nseries, ncol = 18)
 
+# produce new forecasts
 for(i in 1:nseries){
    print(i)
    ets1[i, ] <- forecast(ets(M3[[i]]$x), h = 18, PI = FALSE)$mean
@@ -49,9 +55,12 @@ for(i in 1:nseries){
       # 1154  and a bunch aferhas singular x matrix problem because of trading days 
       #1405 fails for 'unknown reason'
    } else {
-      try(m <- seas(M3[[i]]$x, regression.aictest = NULL, forecast.maxlead = 18)) # about 25% of annual series fail to start
+      try(m <- seas(M3[[i]]$x, 
+                    regression.aictest = NULL, 
+                    regression.variables = c("const"), 
+                    forecast.maxlead = 18)) 
    }
-   try(seats[i, ] <- series(m, "forecast.forecasts")[1:18 , 1]      ) # sometimes generates 36 or more... forecast.maxlead obviously means something i don't understand
+   try(seats[i, ] <- series(m, "forecast.forecasts")[1:18 , 1]) 
 }
 
 
@@ -125,15 +134,25 @@ p1 <- m3table %>%
    mutate(hybrid = ifelse(grepl("Hybrid", method), "Hybrid", "Single")) %>%
    ggplot(aes(x = MASE, y = method, colour = hybrid)) +
    geom_point() +
-   geom_segment(aes(yend = method), xend = 1)
+   geom_segment(aes(yend = method), xend = 1) +
+   labs(x = "Mean Absolute Scaled Error (smaller is better)", colour = "") +
+   ggtitle("Comparison of selected forecasting methods on the M3 data")
+svg("../img/0024-dots.svg", 6, 5)
+print(p1)
+dev.off()
 
+png("../img/0024-dots.png", 600, 500, res = 100)
+print(p1)
+dev.off()
 
 #=================further investigation===============
 
 #-------------example series with unit root--------------
 # Series Y434, Marital status (numbers in thousands), 1974 to 1988
-plot(M3[[434]]$x)
-
+svg("../img/0024-prob1.svg", 6, 4)
+plot(M3[[434]]$x, bty = "l", main = "Y434 Marital status (numbers in thousands)")
+grid()
+dev.off()
 
 # this works and decides it's ARIMA(0,1,0) with drift:
 auto.arima(M3[[434]]$x)
@@ -153,8 +172,11 @@ series(m, "forecast.forecasts")[1:18 , 1]
 aarima[434, ]
 
 #-----------trading day problem--------------
-# Denmark GDP by expenditure
-plot(M3[[1154]]$x)
+# quarterly Denmark GDP by expenditure
+svg("../img/0024-prob2.svg", 6, 4)
+plot(M3[[1154]]$x, bty = "l", main = "Q509 Denmark GDP by expenditure")
+grid()
+dev.off()
 
 # fails because of singularity to do with number of Saturdays:
 seas(M3[[1154]]$x)
@@ -168,3 +190,183 @@ m <- seas(M3[[1154]]$x,
 series(m, "forecast.forecasts")[1:18 , 1]    
 
 aarima[1154, ]
+
+
+#---------------runs but produces no data------
+# quarterly cash flow
+svg("../img/0024-prob3.svg", 6, 4)
+plot(M3[[807]]$x, bty = "l", main = "Q162 Cash Flow" )
+grid()
+dev.off()
+
+# following runs but produces no data
+seas(M3[[807]]$x)
+
+# this works and concludes MA(1):
+auto.arima(M3[[807]]$x)
+
+# but if we manually specify the model
+m <- seas(M3[[807]]$x, 
+          regression.variables = c("const"),
+          arima.model = c(0,0,1), 
+          forecast.maxlead = 18)
+series(m, "forecast.forecasts")[1:18 , 1]  
+aarima[807, ]
+
+
+#---------cannot process spec file for an unknown reason---------
+# Monthly shipments
+svg("../img/0024-prob4.svg", 6, 4)
+plot(M3[[1485]]$x, main = "M84, Shipments")
+grid()
+dev.off()
+
+# runs fine:
+auto.arima(M3[[1485]]$x)
+
+# takes a long time and eventually fails for unknown reason:
+seas(M3[[1485]]$x)
+
+
+# still fails:
+m <- seas(M3[[1485]]$x, 
+          regression.variables = c("const"),
+          arima.model = c(0,1,1), 
+          forecast.maxlead = 18)
+
+# can't work this one out
+
+
+#-----------can't handle phony date/times----------------
+# Telecommunications data, start = "1"
+svg("../img/0024-prob5.svg", 6, 4)
+plot(M3[[3000]]$x, main = "O171 Telecommunication data")
+grid()
+dev.off()
+
+# fails because X13 can't handle abstract times, or things 
+# starting in year 1.  No obvious way around this.
+seas(M3[[3000]]$x)
+
+
+#=================when X13 runs, but does different than auto.arima=============
+# 15th row of mase is for X13 and 9th is for auto arima
+
+
+mases <- data_frame(
+      x13 = mase[15, ],
+      aa = mase[9, ],
+      id = 1:ncol(mase)) %>%
+   mutate(diff = x13 - aa) 
+
+
+#------------X13 much better---------------
+mases %>% arrange(diff)
+# stock shares sold on NYSE 1947 to 1987
+# note that basically the crash of 1987 is what stuffed auto arima
+# X13 seems to make an inspired guess!
+m <- seas(M3[[335]]$x, regression.aictest = NULL, regression.variables = "const")
+summary(m) # ARIMA(1,0,0)
+auto.arima(M3[[335]]$x) # ARIMA(0,2,0)
+
+comp_x13_seats <- function(s){
+            
+   orig <- M3[[s]]$x
+   act  <- M3[[s]]$xx
+   
+   tmp1 <- rbind(
+      data.frame(
+         time = as.numeric(time(orig)),
+         value = as.numeric(orig),
+         method = "Actual result",
+         stringsAsFactors = FALSE),
+      data.frame(
+         time = as.numeric(time(act)),
+         value = as.numeric(act),
+         method = "Actual result",
+         stringsAsFactors = FALSE)
+   )
+   
+   tmp2 <- data_frame(
+      x13 = seats[s, 1:length(act)],
+      aa  = aarima[s, 1:length(act)],
+      time = time(act)
+   ) %>%
+      gather(method, value, -time) 
+      
+   tmp <- rbind(tmp1, tmp2) %>%
+      mutate(method = gsub("aa", "auto.arima()", method),
+             method = gsub("x13", "X13-SEATS", method))
+   
+   # we want a named vector of colours so each method gets the same colour each time
+   pal <- brewer.pal(3, "Set1")
+   names(pal) <- unique(tmp$method)
+   
+   # need the order to help order the legend
+   latest <- tmp %>%
+      filter(time == max(time)) %>%
+      arrange(-value) 
+   
+   p1 <- tmp %>%
+      mutate(method = factor(method, levels = latest$method)) %>%
+      ggplot(aes (x = time, y = value, colour = method)) +
+      geom_line() +
+      geom_point() +
+      scale_y_log10() +
+      ggtitle(M3[[s]]$description) +
+      scale_colour_manual(values = pal)
+   
+   svg(paste0("../img/0024-x13-comp-", s, ".svg"), 6, 3)
+      print(p1)
+   dev.off()
+}
+
+comp_x13_seats(335)
+comp_x13_seats(334)
+comp_x13_seats(332)
+comp_x13_seats(49)
+comp_x13_seats(171)
+
+
+#-----------X13 much worse eg 1------------
+mases %>% arrange(-diff)
+comp_x13_seats(73)
+comp_x13_seats(64)
+comp_x13_seats(1388)
+comp_x13_seats(120)
+
+
+
+plot(M3[[73]]$x)
+
+aarima[73, ]
+seats[73, ] # completely explosive
+
+m <- seas(M3[[73]]$x, 
+          regression.aictest = NULL, 
+          regression.variables = c("const"),
+          forecast.maxlead = 18)
+# SEATS gives ARIMA(1,1,2), log transform
+
+
+# auto.arima gives a simpler ARIMA(0,1,0) without the log transform
+auto.arima(M3[[73]]$x)
+
+
+
+#-----------------X13 much worse eg 2----------------
+mases %>% arrange(-diff)
+
+plot(M3[[1388]]$x)
+
+aarima[1388, ]
+seats[1388, ] # again, completely explosive
+
+m <- seas(M3[[1388]]$x, forecast.maxlead = 18)
+summary(m) # ARIMA(1,1,0)(1,1,0), log transform
+
+
+# auto.arima gives ARIMA(1,1,0)(0,1,0) on original
+auto.arima(M3[[1388]]$x)
+
+
