c15a7229040da6f26c57be8a714d78c2a56816db
ellisp <peter.ellis2013nz@gmail.com>
2016-02-28 18:02:19 +1300

    working version of first three plots of backgammon post

--- _working/0032_xg-export.R
+++ _working/0032_xg-export.R
@@ -1,3 +1,4 @@
+#---------load up functionality and fonts------------
 library(readr)
 library(dplyr)
 library(tidyr)
@@ -5,33 +6,112 @@ library(ggplot2)
 library(showtext)
 library(directlabels) # see http://stackoverflow.com/questions/13627735/stat-contour-with-data-labels-on-lines
 library(caret)        # for cross-validation
-
-
+library(RColorBrewer)
 
 font.add.google("Poppins", "myfont")
 showtext.auto()
 theme_set(theme_light(base_family = "myfont"))
 
+pal <- brewer.pal(7, "Set1")[ 1:2]
+
+#-----------import data----------------
 # Open XG-Gammon, choose "players", "see profile results", "Results",
 # "Copy to clipboard", "sessions list".  Paste result into Excel and save as a csv.
-xg_orig <- read_csv("../data/xg-export.csv")
+# xg_orig <- read_csv("../data/xg-export.csv")
+# or you can use one I've prepared earlier, just with the column of opponent names deleted:
+xg_orig <- read_csv("https://raw.githubusercontent.com/ellisp/ellisp.github.io/source/data/xg-export.csv")
 names(xg_orig) <- gsub(" ", "_", names(xg_orig), fixed = TRUE)
 xg <- xg_orig
 
-# this plot shows that the "Elo_Level" is estimated by XG-Gammon
-ggplot(xg, aes(x = Eq_per_Decision, y = Elo_Level)) +
-          geom_point()
 
-
-# interesting - peer pressure? - they play better and I play better? or is it some impact over time
-ggplot(xg, aes(x = -Eq_per_Decision, y = -Opp_Eq_per_move, size = Match_Length, colour = as.factor(Result))) +
-   geom_point(alpha = 0.2) +
-   scale_x_sqrt() + scale_y_sqrt() +
-   geom_smooth(method = "lm")
-
-
+#--------equity v Elo-----------
 # PR = -(equity error per decision) * 500
 # http://www.bgonline.org/forums/webbbs_config.pl?noframes;read=53424
+# therefore e =  -PR / 500
+
+# make a data frame I'll use for convertin equity per decision to Player Rating
+# (PR) on various plots
+pr_steps <- data.frame(PR = seq(from = 0, to = 40, by = 5)) %>%
+   mutate(Eq_per_Decision = - PR / 500,
+          PR = ifelse(PR == 40, "PR:", as.character(PR)))
+
+
+# this plot shows that the "Elo_Level" is estimated by XG-Gammon
+svg("../img/0032-Elo.svg", 6, 5)
+ggplot(xg, aes(x = Eq_per_Decision, y = Elo_Level)) +
+   geom_point() +
+   scale_y_continuous(limits=c(670, 2100), 
+                      breaks = seq(from = 800, to = 2000, by = 200)) +
+   geom_text(data = pr_steps, y = 700, aes(label = PR), 
+             colour = "grey50", family = "myfont") +
+   labs(x = "Equity change per decision (negative means lost equity)",
+        y = "Equivalent Elo level estimated by XG-Gammon",
+        title = "XG-Gammon estimate of how equity per decision\nshould be related to Elo rating")
+dev.off()
+
+
+#-------------------skill v luck v winning------------------------
+
+svg("../img/0032-luck-v-skill.svg", 6, 6)
+xg %>%
+   mutate(Result = factor(ifelse(Result == 0, "Loss", "Win"),
+                          levels = c("Win", "Loss"))) %>%
+   ggplot(aes(x = Eq_per_Decision, y = Cost_luck, colour = Result)) +
+   geom_point(aes(size = Match_Length), shape = 2) +
+   geom_text(data = pr_steps, y = -1.3, aes(label = PR), 
+             colour = "grey50", family = "myfont") +
+   scale_radius("Match\nlength", breaks = c(1, 3, 5, 7, 9, 11)) +
+   scale_colour_manual(values = pal) +
+   labs(y = "Match equity gained through luck\n-1 = certain loss, +1 = certain win",
+        x = "Skill: equity change per decision\nnegative means equity lost through poor choices",
+        title = "Luck is more important than skill\nin a single backgammon match")
+dev.off()
+
+
+xg_wth_skill <- xg %>%
+   mutate(Result = factor(ifelse(Result == 0, "Loss", "Win"),
+                          levels = c("Win", "Loss")),
+          Opp_Eq_per_Decision = Opp_Eq_per_move * Opp_Roll / Opp_Decisions, 
+          net_skill = Eq_per_Decision - Opp_Eq_per_Decision ) 
+
+p3 <- xg_wth_skill %>%
+   ggplot(aes(x = net_skill, y = Cost_luck, colour = Result)) +
+   geom_vline(xintercept = 0, colour = "grey45") +
+   geom_hline(yintercept = 0, colour = "grey45") +
+   geom_point(aes(size = Match_Length), shape = 2) +
+   scale_radius("Match\nlength", breaks = c(1, 3, 5, 7, 9, 11)) +
+   scale_colour_manual(values = pal) +
+   labs(y = "Match equity gained through luck\n-1 = certain loss, +1 = certain win",
+        x = "Skill: net equity change from both players' decisions\nnegative means net equity loss through poor choices",
+        title = "Luck is more important than skill\nin a single backgammon match") +
+   annotate("text", x = 0.3, y = 0.9, label = "More skilled\nand luckier", 
+            family = "myfont", colour = "grey40") +
+   annotate("text", x = -0.05, y = -1, label = "Less skilled\nand unluckier", 
+            family = "myfont", colour = "grey40") +
+   
+   annotate("text", x = 0.17, y = -0.17, label = "More skilled and\novercame bad luck", 
+            family = "myfont", colour = pal[1]) +
+   annotate("segment", x = 0.11, xend = 0.045, y = -0.17, yend = -0.17, 
+            colour = pal[1], arrow = arrow(angle = 20)) +
+   annotate("segment", x = 0.13, xend = 0.09, y = -0.3, yend = -0.65, 
+            colour = pal[1], arrow = arrow(angle = 20)) +
+   
+   annotate("text", x = 0.17, y = -0.93, label = "More skilled\nbut luck won out", 
+            family = "myfont", colour = pal[2]) +
+   annotate("segment", x = 0.13, xend = 0.06, y = -0.93, yend = -0.93, 
+            colour = pal[2], arrow = arrow(angle = 20))
+   
+
+svg("../img/0032-luck-v-net-skill.svg", 7, 6)
+print(p3)
+dev.off()
+
+
+png("../img/0032-luck-v-net-skill.png", 700, 600, res = 100)
+print(p3)
+dev.off()
+
+#---------------------------luck v skill in modelling----------
 
 xg <- xg_orig %>%
    mutate(pr_me = -Eq_per_Decision * 500,
@@ -44,11 +124,17 @@ ggplot(xg, aes(y = pr_me, x = Result) )+
    coord_flip() 
 
 
-
-
+# xg_doubled <- xg %>%
+#    mutate(tmp = pr_me,
+#           pr_me = pr_opp,
+#           pr_opp = tmp, 
+#           Result = factor(1 - as.numeric(as.character(Result)))) %>%
+#    select(-tmp) %>%
+#    rbind(xg) 
+# 
 #=============prediction over a grid===========
-mod1 <- glm(Result ~  pr_me + pr_opp, family = "binomial", data = xg)
-mod2 <- glm(Result ~  Match_Length + pr_me + pr_opp, family = "binomial", data = xg)
+mod1 <- glm(Result ~  pr_me + pr_opp, family = "binomial", data = xg_doubled)
+mod2 <- glm(Result ~  Match_Length + pr_me + pr_opp, family = "binomial", data = xg_doubled)
 mod3 <- glm(Result ~  Match_Length + pr_me * pr_opp, family = "binomial", data = xg)
 mod4 <- glm(Result ~  Match_Length * pr_me * pr_opp, family = "binomial", data = xg)
 mod5 <- glm(Result ~  Match_Length * (pr_me + pr_opp), family = "binomial", data = xg)
