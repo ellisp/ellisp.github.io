e70d0dc5e07d0df3bd15cec45bf82d609e5a4864
Peter Ellis <peter.ellis2013nz@gmail.com>
2016-06-30 21:31:37 +1200

    minor tweak to final version of inequality post

--- _working/0047-ehii.R
+++ _working/0047-ehii.R
@@ -11,15 +11,17 @@ download.file("http://utip.lbj.utexas.edu/data/EHII-UPDATED-10-30-2013.xlsx",
               destfile = "ehii.xlsx", mode = "wb")
 
 ehii <- read.xlsx("ehii.xlsx")[ , -1] # don't need the first column
-    
+
+
 ehii_tidy <- ehii %>%
    gather(Year, Gini, -Country, -Code) %>%
    mutate(Year = as.numeric(Year))
 
+svg("../img/0047-plain.svg", 7, 5)    
 ggplot(ehii_tidy, aes(x = Year, y = Gini, colour = Country)) +
    geom_line() +
    theme(legend.position = "none")
-
+dev.off()
 
 full_countries <- ehii_tidy %>%
    filter(Year = min(Year) & !is.na(Gini))
@@ -31,21 +33,28 @@ final_result <- ehii_tidy %>%
    filter(Year == max(Year)) %>%
    mutate(Year = Year + 1,
           label = paste(Code, round(Gini)))
-
-
-ehii_tidy %>%
+s
+p1 <- ehii_tidy %>%
    filter(Country %in% full_countries$Country) %>%
    ggplot(aes(x = Year, y = Gini, colour = Country)) +
    stat_index(index.ref = 1, alpha = 0.3) +
    theme(legend.position = "none") +
    geom_text(data = final_result, aes(label = label, y = Gini_index)) +
-   ggtitle("Biggest increases in inequality since 1963",
+   ggtitle("Relative changes in inequality since 1963",
            subtitle = "(for countries with data from 1963)") +
    labs(y = "Index of Gini coefficient, set to be 100 in 1963",
         x = "", caption = "UTIP Estimated Household Income Inequality dataset") +
    xlim(1960, 2012) +
-   annotate("text", x = 1970, y = 150, 
+   annotate("text", x = 1977, y = 150, 
             label = "Country codes appear at their final data point; 
 numbers are the latest available Gini coefficient")
 
 
+svg("../img/0047-gini-growth.svg", 9, 7)
+print(p1)
+dev.off()
+
+
+png("../img/0047-gini-growth.png", 900, 700, res = 100)
+print(p1)
+dev.off()
