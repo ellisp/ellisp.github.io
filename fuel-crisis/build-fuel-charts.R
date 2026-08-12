c0966c01908d1eafd052524383c1042be29c9a38
Peter Ellis <peter.ellis2013nz@gmail.com>
2026-08-13 06:47:48 +1100

    new oil data:

--- fuel-crisis/build-fuel-charts.R
+++ fuel-crisis/build-fuel-charts.R
@@ -18,7 +18,10 @@ source("0327-us-oil-inventories.R")
 source("0328-pacific-cpi.R")
 source("0329-nz-us-petrol.R")
 
+setwd(here())
 system2("jekyll", "build")
 
 # Then in terminal naviage to _site and use gitk to check if anything has
 # changed or just 'date accessed'.
+
+setwd(here("_working"))
